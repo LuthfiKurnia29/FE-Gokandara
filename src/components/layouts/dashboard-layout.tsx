6a55afaadc44df9c0ce976f2a4ff12756c356467
Andino Ferdiansah <andinoferdiansah@gmail.com>
2025-07-08 11:42:33 +0700

    add charts to index

--- src/components/layouts/dashboard-layout.tsx
+++ src/components/layouts/dashboard-layout.tsx
@@ -1,28 +1,21 @@
 'use client';
 
+import type React from 'react';
+
 import { AppSidebar } from '@/components/app-sidebar';
 import { ScrollArea } from '@/components/ui/scroll-area';
-import { SidebarInset, SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar';
+import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar';
 
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator
-} from '../ui/breadcrumb';
-import { Separator } from '../ui/separator';
 import { DashboardHeader } from './dashboard-header';
 
 export function DashboardLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className='min-h-screen flex-1 bg-gray-100' suppressHydrationWarning>
+    <div className='min-h-screen flex-1 bg-gray-50' suppressHydrationWarning>
       <SidebarProvider>
         <AppSidebar />
-        <SidebarInset className='grid h-screen grid-rows-[75px_calc(100vh-75px)]'>
+        <SidebarInset className='grid h-screen grid-rows-[64px_calc(100vh-64px)]'>
           <DashboardHeader />
-          <ScrollArea className='flex flex-1 flex-col gap-4 p-4 pt-0'>{children}</ScrollArea>
+          <ScrollArea className='flex flex-1 flex-col gap-4 p-6 pt-0'>{children}</ScrollArea>
         </SidebarInset>
       </SidebarProvider>
     </div>
