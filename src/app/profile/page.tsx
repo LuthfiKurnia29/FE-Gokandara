4bed7f3ae86e33f0c23e58d1c6e5e3cd7f9ce3d5
wahyall278 <wahyall278@gmail.com>
2025-10-09 20:58:31 +0700

    Feat: transaksi, projek

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,13 +1,9 @@
 import { memo } from 'react';
 
-const ProfilePage = memo(() => {
-  return (
-    <main className='flex min-h-screen items-center justify-center bg-white'>
-      <h1 className='text-2xl font-bold' tabIndex={0} aria-label='Profile Page'>
-        Profile Page
-      </h1>
-    </main>
-  );
+import ProfilePage from '@/blocks/profile';
+
+const Profile = memo(() => {
+  return <ProfilePage />;
 });
 
-export default ProfilePage;
+export default Profile;
